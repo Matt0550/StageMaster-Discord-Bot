47088ed9bee33dbd4389e1d2bbaa9810f75a552b
Matt0550 <65899974+Matt0550@users.noreply.github.com>
2021-04-06 16:10:00 +0200

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,35 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import CommandNotFound
+import traceback
+import sys
+
+bot = commands.Bot(command_prefix=".", intents=discord.Intents.all())
+
+bot.remove_command("help")
+
+DISCORD_TOKEN = 'Insert Discord token here'
+
+initial_extensions = ['cogs.stage']
+
+if __name__ == '__main__':
+    for extension in initial_extensions:
+        try:
+            bot.load_extension(extension)
+        except Exception as e:
+            print(f'Failed to load extension {extension}.', file=sys.stderr)
+            traceback.print_exc()
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, CommandNotFound):
+        await ctx.send("Command Not Found!", delete_after=2)
+        return
+    raise error
+bot.run(DISCORD_TOKEN)
